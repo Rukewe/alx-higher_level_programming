2aaa443de5a40bd7cd8b1df43ffc51edb03bd114
Rukewe <rukewe.obominuru@gmail.com>
2022-05-31 12:53:00 -0700

    task 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str_split = str.spilt()
+print(f"{str_split[5]} {str_split[6]} {str_split[12]} {str_split[0]}")
